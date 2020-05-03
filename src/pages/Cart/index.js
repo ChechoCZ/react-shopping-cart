09b96b2436d7bfbe9dd0aacb88bab7544e951fb3
Sergio Cuadros <sergio@codwise.com>
2020-05-03 16:53:36 -0300

    feat(styling): Create app. Add styling. Configure Redux store.

---
+++ src/pages/Cart/index.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { MdRemoveCircleOutline, MdAddCircleOutline, MdDelete } from 'react-icons/md';
+
+import { Container, ProductTable, Total } from './styles';
+
+function Cart() {
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>PRODUCT</th>
+            <th>QUANTITY</th>
+            <th>SUBTOTAL</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img src="" alt="Shoes"/>
+            </td>
+            <td>
+              <strong>Nice shoes</strong>
+              <span>$129.90</span>
+            </td>
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline size={20} color="#7159c1" />
+                </button>
+                <input type="number" readOnly value={2} />
+                <button type="button">
+                  <MdAddCircleOutline size={20} color="#7159c1" />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>$258.80</strong>
+            </td>
+            <td>
+              <button type="button">
+                <MdDelete size={20} color="#7159c1" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+
+      <footer>
+        <button type="button">Finish</button>
+
+        <Total>
+          <span>Total</span>
+          <strong>$1920.20</strong>
+        </Total>
+      </footer>
+    </Container>
+  );
+}
+
+export default Cart;
